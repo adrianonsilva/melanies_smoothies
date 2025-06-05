42d851fa8a943657df218971aaeb2e23f7615a4c
Adriano N Silva <adriano.silva@gmail.com>
2025-06-05 20:22:27 -0300

    fix api call values

--- streamlit_app.py
+++ streamlit_app.py
@@ -26,8 +26,8 @@ if ingredients_list:
     ingredients_string = ''
     for fruit_chosen in ingredients_list:
         ingredients_string += fruit_chosen + ' '
-        st.subheader(f'{fruit_chosen} Nutrition Information')
-        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/watermelon/{fruit_chosen}")
+        st.subheader(f"{fruit_chosen} Nutrition Information")
+        smoothiefroot_response = requests.get(f"https://my.smoothiefroot.com/api/fruit/{fruit_chosen}")
         st_df = st.dataframe(data = smoothiefroot_response.json(), use_container_width=True)
 
     #st.write(ingredients_string)
