5a9a40be8c0ab645f2e1c0758406cb2a2b1b3562
Adriano N Silva <adriano.silva@gmail.com>
2026-01-30 23:50:07 -0300

    add Exception

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,15 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
+try:
+    cnx = st.connection("snowflake")
+except Exception as err:
+    st.write(
+            """
+            Snowflake connection error!
+        """)    
+    st.stop()
+
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
@@ -12,11 +21,6 @@ st.write(
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be: ", name_on_order)
 
-try:
-    cnx = st.connection("snowflake")
-except ValueError:
-    st.stop()
-    
 session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'), col('SEARCH_ON'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
