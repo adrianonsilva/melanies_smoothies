9769c6aa88569bf96023954b5547c67f7861269e
Adriano N Silva <adriano.silva@gmail.com>
2026-01-30 23:52:06 -0300

    new message error.

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,20 +3,20 @@ import streamlit as st
 from snowflake.snowpark.functions import col
 import requests
 
-try:
-    cnx = st.connection("snowflake")
-except Exception as err:
-    st.write(
-            """
-            Snowflake connection error!
-        """)    
-    st.stop()
-
 # Write directly to the app
 st.title(f":cup_with_straw: Customize Your Smoothie :cup_with_straw:")
 st.write(
   """Choose the fruits you want in your Smoothie!
   """)
+
+try:
+    cnx = st.connection("snowflake")
+except Exception as err:
+    st.write(
+            """
+            Snowflake connection unavailable.!
+        """)    
+    st.stop()
 
 name_on_order = st.text_input("Name on Smoothie:")
 st.write("The name on your Smoothie will be: ", name_on_order)
